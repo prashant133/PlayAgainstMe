dab8d0be4ec1f982a61535c860fe770ae4fcee36
prashant133 <dsrprasant@gmail.com>
2025-11-26 14:08:35 +0545

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-10xl font-bold underline text-green-500">
+        hello play againt me
+      </h1>
+    </>
+  );
+}
+
+export default App;
